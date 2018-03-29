70d51b07fd444d1a5dad95e16a1800ae3ebe701d
agarzonp <agarzonp@gmail.com>
2018-03-29 23:09:49 +0200

    tidy up test environment

--- PhysicsEngine/src/TestEnvironment/TestEnvironment.h
+++ PhysicsEngine/src/TestEnvironment/TestEnvironment.h
@@ -3,6 +3,7 @@
 
 #include <cassert>
 #include <memory>
+#include <list>
 
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/quaternion.hpp"
@@ -18,6 +19,7 @@
 #include "Render/Meshes/SphereMesh.h"
 #include "Render/Renderable.h"
 #include "Render/SphereRenderable.h"
+#include "Render/RenderUtils.h"
 
 #include "Physics/PhysicsEngine.h"
 
@@ -37,6 +39,12 @@ public:
 
 	void OnKeyPressed(int key) override 
 	{
+		switch (key)
+		{
+		case GLFW_KEY_C:
+			PhysicsEngine::s_renderColliders = !PhysicsEngine::s_renderColliders;
+			break;
+		}
 	}
 
 	void OnKeyReleased(int key) override { }
@@ -60,11 +68,15 @@ public:
 		// use the shader
 		shader.Use();
 
+		// render game objects
 		const glm::mat4& viewProjection = camera.ViewProjectionMatrix();
 		for (auto& gameObject : gameObjects)
 		{
 			gameObject.Render(viewProjection);
 		}
+
+		// render physics
+		physicsEngine.DebugRender(viewProjection);
 	}
 	
 protected:
@@ -73,25 +85,34 @@ protected:
 	{
 		// init cube mesh
 		cubeMesh.Init();
+		sphereMesh.Init();
 
 		// load shader
 		shader.Load("assets/Shaders/basic.vert", "assets/Shaders/basic.frag");
 
-		// init game objects
-		InitGameObjects();
+		RenderUtils::InitCubeRenderable(shader, &cubeMesh);
+		RenderUtils::InitSphereRenderable(shader, &sphereMesh);
 
 		// init camera
-		camera.Init(glm::vec3(0.0f, 1.0f, -15.0f), glm::vec3(0.0f, 0.0f, 0.0f), 45.0f, 1024.0f / 768.0f, 0.1f, 1000000.0f);
+		camera.Init(glm::vec3(-20.0f, 5.0f, 20.0f), glm::vec3(0.0f, 0.0f, 0.0f), 45.0f, 1024.0f / 768.0f, 0.1f, 1000000.0f);
 
-		// init physics
-		InitPhysics();
+		// init physics engine
+		physicsEngine.Init();
+
+		// init game objects
+		InitGameObjects();
 	}
 		
 	void InitGameObjects()
 	{
-		GameObject gameObject;
+		InitFloor();
+		InitWalls();
+		InitMovableObjects();
+	}
 
-		// floor
+	void InitFloor()
+	{
+		GameObject gameObject;
 		gameObject.transform.position = glm::vec3(0.0f, 0.0f, 10.0f);
 		gameObject.transform.scale = glm::vec3(40.0f, 0.0001f, 40.f);
 		gameObject.SetRenderable(std::make_shared<Renderable>(&cubeMesh, shader, glm::vec4(0.8f, 0.8f, 0.8f, 1.0f)));
@@ -99,7 +120,22 @@ protected:
 
 		gameObjects.emplace_back(gameObject);
 
-		// walls
+		PhysicObjectDesc physicObject;
+		physicObject.type = PhysicObjectType::PARTICLE;
+		physicObject.mass = 0.0f; // static object
+		physicObject.isAffectedByGravity = false;
+
+		MathGeom::Vector3 pointA = glm::vec3(1.0f, 0.0001f, 0.0f);
+		MathGeom::Vector3 pointB = glm::vec3(-1.0f, 0.0001f, 0.0f);
+		MathGeom::Vector3 pointC = glm::vec3(0.0f, 0.0001f, 0.5f);;
+		physicObject.colliderDesc = std::make_unique<PlaneColliderDesc>(pointA, pointB, pointC, gameObjects.back().transform);
+
+		physicsEngine.AddPhysics(gameObjects.back(), physicObject);
+	}
+
+	void InitWalls()
+	{
+		GameObject gameObject;
 		gameObject.transform.position = glm::vec3(40.0f, 5.0f, 10.0f);
 		gameObject.transform.scale = glm::vec3(0.5f, 5.0f, 40.f);
 		gameObject.SetRenderable(std::make_shared<Renderable>(&cubeMesh, shader, glm::vec4(0.11f, 0.11f, 0.11f, 1.0f)));
@@ -127,71 +163,55 @@ protected:
 		gameObject.SetVisible(true);
 
 		gameObjects.emplace_back(gameObject);
-
-		// green sphere
-
-		gameObject.transform.position = glm::vec3(-20.0f, 5.5f, 40.0f);
-		gameObject.transform.rotation = glm::vec3(0.5f, 0.5f, 0.0f);
-		gameObject.transform.scale = glm::vec3(1.0f, 1.0f, 1.0f);
-		gameObject.SetRenderable(std::shared_ptr<Renderable>(static_cast<Renderable*>(new SphereRenderable(3.0f, &sphereMesh, shader, glm::vec4(0.0f, 0.3f, 0.0f, 1.0f)))));
-		gameObject.SetVisible(true);
-
-		gameObjects.emplace_back(gameObject);
-
-		gameObject.transform.position = glm::vec3(25.0f, 5.5f, 40.0f);
-		gameObject.transform.rotation = glm::vec3(0.5f, 0.5f, 0.0f);
-		gameObject.transform.scale = glm::vec3(1.0f, 1.0f, 1.0f);
-		gameObject.SetRenderable(std::shared_ptr<Renderable>(static_cast<Renderable*>(new SphereRenderable(5.0f, &sphereMesh, shader, glm::vec4(0.0f, 0.3f, 0.0f, 1.0f)))));
-		gameObject.SetVisible(true);
-
-		gameObjects.emplace_back(gameObject);
-
-		// red cube
-
-		gameObject.transform.position = glm::vec3(25.0f, 2.5f, 40.0f);
-		gameObject.transform.rotation = glm::vec3(0.5f, 0.5f, 0.0f);
-		gameObject.transform.scale = glm::vec3(2.5f, 2.5f, 2.5f);
-		gameObject.SetRenderable(std::make_shared<Renderable>(&cubeMesh, shader, glm::vec4(0.5f, 0.0f, 0.0f, 1.0f)));
-		gameObject.SetVisible(true);
-
-		gameObjects.emplace_back(gameObject);
 	}
 
-	void InitPhysics()
+	void InitMovableObjects()
 	{
-		physicsEngine.Init();
+		// green object
+		GameObject greenObject;
+		greenObject.transform.position = glm::vec3(-20.0f, 5.5f, 40.0f);
+		greenObject.transform.rotation = glm::vec3(0.0f, 0.0f, 0.0f);
+		greenObject.transform.scale = glm::vec3(2.0f, 2.0f, 2.0f);
+		float sphereRadius = greenObject.transform.scale.x;
+		greenObject.SetRenderable(std::shared_ptr<Renderable>(static_cast<Renderable*>(new SphereRenderable(sphereRadius, &sphereMesh, shader, glm::vec4(0.0f, 0.3f, 0.0f, 1.0f)))));
+		//gameObject.SetRenderable(std::make_shared<Renderable>(&cubeMesh, shader, glm::vec4(0.0f, 0.3f, 0.0f, 1.0f)));
+		greenObject.SetVisible(true);
 
-		// green sphere
-		PhysicObjectDesc desc5;
-		desc5.type = PhysicObjectType::PARTICLE;
-		desc5.mass = 10.0f;
-		desc5.velocity = MathGeom::Vector3(2.0f, 0.0f, 0.0f);
-		desc5.acceleration = MathGeom::Vector3(10.0f, 0.0f, 0.0f);
-		//desc5.colliderDesc = std::make_unique<SphereColliderDesc>(3.0f, gameObjects[5].transform);
-		desc5.colliderDesc = std::make_unique<AABBColliderDesc>(MathGeom::Vector3(1.5f, 1.5f, 1.5f), gameObjects[5].transform);
-		desc5.isAffectedByGravity = false;
+		gameObjects.emplace_back(greenObject);
 
-		physicsEngine.AddPhysics(gameObjects[5], desc5);
+		PhysicObjectDesc greenPhysicObject;
+		greenPhysicObject.type = PhysicObjectType::PARTICLE;
+		greenPhysicObject.mass = 10.0f;
+		greenPhysicObject.velocity = MathGeom::Vector3(0.0f, 0.0f, 0.0f);
+		greenPhysicObject.acceleration = MathGeom::Vector3(0.0f, 0.0f, 0.0f);
+		greenPhysicObject.colliderDesc = std::make_unique<SphereColliderDesc>(gameObjects.back().transform);
+		//desc5.colliderDesc = std::make_unique<AABBColliderDesc>(gameObjects.back().transform);
+		greenPhysicObject.isAffectedByGravity = true;
 
-		PhysicObjectDesc desc6;
-		desc6.type = PhysicObjectType::PARTICLE;
-		desc6.mass = 10.0f;
-		desc6.velocity = MathGeom::Vector3(-2.0f, 0.0f, 0.0f);
-		desc6.acceleration = MathGeom::Vector3(-10.0f, 0.0f, 0.0f);
-		desc6.colliderDesc = std::make_unique<SphereColliderDesc>(5.0f, gameObjects[6].transform);
-		//desc6.colliderDesc = std::make_unique<AABBColliderDesc>(MathGeom::Vector3(2.5f, 2.5f, 2.5f), gameObjects[6].transform);
-		desc6.isAffectedByGravity = false;
+		physicsEngine.AddPhysics(gameObjects.back(), greenPhysicObject);
 
-		physicsEngine.AddPhysics(gameObjects[6], desc6);
-		
-		// red cube
-		PhysicObjectDesc desc7;
-		desc7.type = PhysicObjectType::PARTICLE;
-		desc7.mass = 10.0f;
-		desc7.velocity = MathGeom::Vector3(-2.0f, 0.0f, 0.0f);
-		desc7.acceleration = MathGeom::Vector3(-10.0f, 0.0f, 0.0f);
+		// red object
+		GameObject redObject;
+		redObject.transform.position = glm::vec3(25.0f, 5.5f, 40.0f);
+		redObject.transform.rotation = glm::vec3(0.0f, 0.0f, 0.0f);
+		redObject.transform.scale = glm::vec3(3.0f, 3.0f, 3.0f);
+		sphereRadius = redObject.transform.scale.x;
+		//redObject.SetRenderable(std::shared_ptr<Renderable>(static_cast<Renderable*>(new SphereRenderable(sphereRadius, &sphereMesh, shader, glm::vec4(0.0f, 0.3f, 0.0f, 1.0f)))));
+		redObject.SetRenderable(std::make_shared<Renderable>(&cubeMesh, shader, glm::vec4(0.5f, 0.0f, 0.0f, 1.0f)));
+		redObject.SetVisible(true);
 
-		//physicsEngine.AddPhysics(gameObjects[7], desc7);
+		gameObjects.emplace_back(redObject);
+
+		PhysicObjectDesc redPhysicObject;
+		redPhysicObject.type = PhysicObjectType::PARTICLE;
+		redPhysicObject.mass = 10.0f;
+		redPhysicObject.velocity = MathGeom::Vector3(-0.8f, 0.0f, 0.0f);
+		redPhysicObject.acceleration = MathGeom::Vector3(-0.8f, 0.0f, 0.0f);
+		//redPhysicObject.colliderDesc = std::make_unique<SphereColliderDesc>(gameObjects.back().transform);
+		redPhysicObject.colliderDesc = std::make_unique<AABBColliderDesc>(gameObjects.back().transform);
+		redPhysicObject.isAffectedByGravity = false;
+
+		physicsEngine.AddPhysics(gameObjects.back(), redPhysicObject);
 	}
 
 	void Terminate()
@@ -213,7 +233,7 @@ private:
 	FreeCamera camera;
 
 	// game objects
-	std::vector<GameObject> gameObjects;
+	std::list<GameObject> gameObjects;
 
 	// Physics engine
 	PhysicsEngine physicsEngine;
